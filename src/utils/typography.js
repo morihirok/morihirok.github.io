9c40f2f2d46a131f7822bc53458a6e6b61da3191
morihirok <omnimani3@gmail.com>
2018-08-17 19:20:03 +0900

    first commit

---
+++ src/utils/typography.js
@@ -0,0 +1,26 @@
+import Typography from 'typography'
+import Wordpress2016 from 'typography-theme-wordpress-2016'
+
+Wordpress2016.overrideThemeStyles = () => ({
+  'a.gatsby-resp-image-link': {
+    boxShadow: 'none',
+  },
+})
+
+delete Wordpress2016.googleFonts
+
+Wordpress2016.headerFontFamily = [
+  'Merriweather',
+  'Georgia',
+  'YuGothic',
+  'sans-serif',
+]
+
+const typography = new Typography(Wordpress2016)
+
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== 'production') {
+  typography.injectStyles()
+}
+
+export default typography
