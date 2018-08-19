1b900e7308a5ba40421425ae5e9177f5ee1542b7
morihirok <omnimani3@gmail.com>
2018-08-20 01:15:03 +0900

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-1c2dcaad9c435d369737.js","5ffb26919d9904799b314840b2ffd0b0"],["/app-80d7d0350b146ea0a37f.js","8833cdfbf42f2fb0de2b867bd37dbbc7"],["/app-a42ea5b2a84e53cc797c.js","fd76ad4d42cfe1bb37d4e583b079aa47"],["/commons-39b20ebe64a525b95219.js","fd500a98a08db6905ccd69738b8c1390"],["/index.html","f400e93d04aba41e14402abd41402553"],["/offline-plugin-app-shell-fallback/index.html","c09c977b52f35d1b66819b20fb18cb1c"]];
+var precacheConfig = [["/app-1c2dcaad9c435d369737.js","5ffb26919d9904799b314840b2ffd0b0"],["/app-80d7d0350b146ea0a37f.js","8833cdfbf42f2fb0de2b867bd37dbbc7"],["/app-a42ea5b2a84e53cc797c.js","fd76ad4d42cfe1bb37d4e583b079aa47"],["/commons-39b20ebe64a525b95219.js","fd500a98a08db6905ccd69738b8c1390"],["/index.html","3fb66a7015d79f0c3fd644c3af0bbce0"],["/offline-plugin-app-shell-fallback/index.html","a7e85df2d1a14b50700028e7d1eccb4c"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
